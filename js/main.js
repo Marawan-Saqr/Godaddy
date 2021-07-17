0ea0b2ec402578eee34d32f49168e644fa8c55e2
Marawan-Saqr <loew.krs@yahoo.com>
2021-07-17 05:08:00 +0200

    Testing

--- js/main.js
+++ js/main.js
@@ -15,6 +15,63 @@ let showMore = () => {
 
 readMore.addEventListener("click", showMore);
 //==========================================================//
+// scrolltop function for section menu
+
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        x = $(window).scrollTop();
+        console.log(x);
+    })
+     
+
+    // .menu .left ul .first
+    $(window).scroll(function(){
+       if($(window).scrollTop() > 914) {
+           $(".menu").css({
+            position: "fixed",
+            top: "0",
+            right: "0",
+            left: "0",
+            zIndex: "1030",
+            background: "rgb(251 238 238)",
+           });
+
+           $(".menu .left ul .first").css("border-bottom", "1px solid aqua");
+           
+
+       } else if($(window).scrollTop() < 914){
+            $(".menu").css("position", "static");
+            $(".menu .left ul .first").css("border-bottom", "none");
+            $(".menu").css({
+                background: "transparent",
+                paddingTop: "5px",
+            })
+       }
+    });
+
+
+    //.menu .left ul .second
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 1882) {
+            $(".menu .left ul .first").css("border-bottom", "none");
+            $(".menu .left ul .second").css("border-bottom", "1px solid aqua");
+
+        } else if ($(window).scrollTop() < 1882) {
+            $(".menu .left ul .second").css("border-bottom", "none");
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+}); // document
 
 
 
@@ -48,3 +105,9 @@ readMore.addEventListener("click", showMore);
 
 // plugins
 new WOW().init();        // wow.js
+
+var scroll = new SmoothScroll('a[href*="#"]');     // smooth scroll
+
+$(document).ready(function(){                     // owl.js
+    $('.owl-carousel').owlCarousel();
+  });
